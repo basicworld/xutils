eef356726d0bac6dcac48fcfa96dced6a493126c
basicworld <basicworld@163.com>
2016-07-02 23:51:45 +0800

    debug

--- xbuild.py
+++ xbuild.py
@@ -13,8 +13,8 @@ def builDir(dirName='./', fileName=''):
     create dir to make sure os.path.isdir(dirName) = True
     """
     # dirName = os.path.abspath(dirName)
-    dirName = unicode(dirName)
-    fileName = unicode(fileName)
+    # dirName = unicode(dirName)
+    # fileName = unicode(fileName)
     if not os.path.isdir(dirName):
         os.makedirs(dirName)
     return(os.path.join(dirName, fileName) if fileName else dirName)
